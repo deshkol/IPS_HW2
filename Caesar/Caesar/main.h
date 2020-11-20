ddf4de6999edbda2df99e37d8c3c47f979cdc94f
leosegre <58905206+leosegre@users.noreply.github.com>
2020-11-20 11:40:46 +0200

    initial

---
+++ Caesar/Caesar/main.h
@@ -0,0 +1,18 @@
+#ifndef __main_h__
+#define __main_h__
+
+//includes
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+//functions
+int main(int argc, char* argv[]);
+
+//define
+
+
+
+#endif // __main_h__
